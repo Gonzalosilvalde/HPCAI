249eb1a1e3cd3e980c439d570650a239280fbaa1
Unai <iborra.unai@gmail.com>
2025-10-06 15:16:59 +0200

    reverted profiling fn

--- profiling.py
+++ profiling.py
@@ -1,15 +1,11 @@
 from torch.profiler import profile, record_function, ProfilerActivity
-from torch.utils.tensorboard import SummaryWriter
-import os
 
 def start_profiling(func, log_dir="./profile"):
-    os.makedirs(log_dir, exist_ok=True)
     with profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
         record_shapes=True,
-        with_stack=True,
-        on_trace_ready=torch.profiler.tensorboard_trace_handler(log_dir),
+        with_stack=True
     ) as prof:
         with record_function("model_training"):
             func()
-        prof.step()
+    prof.export_chrome_trace(f"{log_dir}/trace.json")
