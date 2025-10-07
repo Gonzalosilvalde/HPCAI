f6ce490cd2ce6d7141dd8a70c7f64b1359a578cc
Gonzalo <gonzalo.silvalde@gmail.com>
2025-10-07 18:07:39 +0200

    requirements.txt added

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -14,15 +14,4 @@ source ./venv/bin/activate
 # Los he sacado del ejemplo del CFR24
 echo "Installing modules..."
 
-python3 -m pip install tensorflow[and-cuda]
-pip install jupyterlab-nvdashboard
-pip install -U tensorboard-plugin-profile
-pip install nvidia-pyindex
-pip install nvidia-cuda-cupti
-pip install matplotlib
-pip install portpicker
-pip install tensorflow-datasets
-
-# Estos me dice chatgpt que los use tb
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip install transformers datasets tqdm tensorboard matplotlib portpicker
+pip install -r requirements.txt
