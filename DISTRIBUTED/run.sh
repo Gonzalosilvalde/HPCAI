3704ca3871a06cdace141ce65cddf573a1d03927
Unai <iborra.unai@gmail.com>
2025-11-24 12:10:46 +0100

    fixed run.sh

--- DISTRIBUTED/run.sh
+++ DISTRIBUTED/run.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=bert_test
 #SBATCH --output=bert_test_a100.out
 #SBATCH --error=bert_test_a100.err
-#SBATCH --time=02:30:00
+#SBATCH --time=01:00:00
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=32G
-#SBATCH --ntasks=2
+#SBATCH --ntasks=1
 
 source ./.env
 module load cesga/2025
